03e2cda0f79cfa59a45a014c9b3c504baf6970fa
Tomáš Ondruško <485418@mail.muni.cz>
2020-11-11 21:34:08 +0100

    UI init

---
+++ src/main/java/jate/ui/Dashboard.java
@@ -0,0 +1,24 @@
+package jate.ui;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Dashboard {
+    private final JFrame frame;
+
+    public Dashboard() {
+        frame = createFrame();
+    }
+
+    public void show() {
+        frame.setVisible(true);
+    }
+
+    private JFrame createFrame() {
+        var frame = new JFrame("Hotel Reservation System");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(1000, 700);
+        return frame;
+    }
+
+}
