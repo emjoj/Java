f308c25158fdbb2f0cd8c34c9a6a868f3856513a
Ema Stefanikova <x467942@fi.muni.cz>
2020-12-01 00:02:36 +0100

    add methods to PersonDao

--- src/main/java/cz/muni/fi/pv168/jate/hotelreservationsystem/data/PersonDao.java
+++ src/main/java/cz/muni/fi/pv168/jate/hotelreservationsystem/data/PersonDao.java
@@ -1,10 +1,10 @@
 package cz.muni.fi.pv168.jate.hotelreservationsystem.data;
 
-import cz.muni.fi.pv168.jate.hotelreservationsystem.data.Person;
-
 import javax.sql.DataSource;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
@@ -18,7 +18,7 @@ public class PersonDao {
 
     public void create(Person person) {
         if (person.getId() != null) {
-            throw new IllegalArgumentException("Employee already has ID: " + person);
+            throw new IllegalArgumentException("Person already has ID: " + person);
         }
         try (var connection = dataSource.getConnection();
              var st = connection.prepareStatement(
@@ -29,7 +29,8 @@ public class PersonDao {
             st.setDate(3, Date.valueOf(person.getBirthDate()));
             var reservation = person.getReservation();
 
-            /*to work reservation class needs to be created and
+            /*to work reservation class needs to be created
+
             if (reservation == null) {
                 st.setNull(4, Types.BIGINT);
             } else {
@@ -41,13 +42,54 @@ public class PersonDao {
                 if (rs.next()) {
                     person.setId(rs.getLong(1));
                 } else {
-                    throw new DataAccessException("Failed to fetch generated key: no key returned for employee: " + person);
+                    throw new DataAccessException("Failed to fetch generated key: no key returned for person: " + person);
                 }
             }
 
 
         } catch (SQLException ex) {
-            throw new DataAccessException("Failed to store employee " + person, ex);
+            throw new DataAccessException("Failed to store person" + person, ex);
+        }
+    }
+
+    public List<Person> findAll() {
+        try (var connection = dataSource.getConnection();
+             var st = connection.prepareStatement("SELECT ID, FIRST_NAME, LAST_NAME, BIRTH_DATE, RESERVATION_ID FROM PERSON")) {
+
+            List<Person> persons = new ArrayList<>();
+            try (var rs = st.executeQuery()) {
+                while (rs.next()) {
+                    Person person = new Person(
+                            rs.getString("FIRST_NAME"),
+                            rs.getString("LAST_NAME"),
+                            rs.getDate("BIRTH_DATE").toLocalDate());
+                    person.setId(rs.getLong("ID"));
+                    var reservationId = rs.getLong("RESERVATION_ID");
+                    /*if (!rs.wasNull())*/
+                    // reservationDao needs to be created to work
+                    //{person.setReservation(reservationDao.findById(reservationId)); }
+                    persons.add(person);
+                }
+            }
+            return persons;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to load all persons", ex);
+        }
+    }
+
+    private void createTable() {
+        try (var connection = dataSource.getConnection();
+             var st = connection.createStatement()) {
+
+            st.executeUpdate("CREATE TABLE APP.PERSON (" +
+                    "ID BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY," +
+                    "FIRST_NAME VARCHAR(100) NOT NULL," +
+                    "LAST_NAME VARCHAR(100) NOT NULL," +
+                    "BIRTH_DATE DATE NOT NULL," +
+                    "RESERVATION_ID BIGINT REFERENCES APP.RESERVATION(ID)" +
+                    ")");
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to create PERSON table", ex);
         }
     }
 }
